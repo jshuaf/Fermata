380f6f946de2283d3216f0fa0b8914662ab768b7
Joshua <joshuacfang@gmail.com>
2016-11-18 07:44:33 -0500

    Separated into two pages.

--- Fermata/Extensions.swift
+++ Fermata/Extensions.swift
@@ -7,12 +7,31 @@
 //
 
 import Foundation
+import UIKit
 
 struct Helper {
 	static func getDocumentsDirectory() -> URL {
 		let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
 		let documentsDirectory = paths[0]
 		return documentsDirectory
-
 	}
+
+	static func getRecordingsDirectory() -> URL? {
+		let recordingsDirectory = Helper.getDocumentsDirectory().appendingPathComponent("Recordings")
+		do {
+			try FileManager.default.createDirectory(at: recordingsDirectory, withIntermediateDirectories: true, attributes: nil)
+			return recordingsDirectory
+		} catch {
+			print("\(error)")
+			return nil
+		}
+	}
+}
+
+extension UILabel {
+	var substituteFontName: String {
+		get { return self.font.fontName }
+		set { self.font = UIFont(name: newValue, size: self.font.pointSize) }
+	}
+
 }
